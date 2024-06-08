c2367d8b5f240c7116c2d44b448be5a00972e3f6
joelsrzalvarez <contactojoelsuarez@gmail.com>
2024-06-08 22:04:44 +0200

    navbar fixed now always is present while you are scrolling

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -10,17 +10,13 @@ export const Hero = () => {
         <h1 className={styles.title}>Hi, I'm Joel</h1>
         <p className={styles.description}>
           I'm a full-stack developer with almost 2 years of experience using JavaScript. 
-          Reach out if you'd like to learn more!
+          Reach out if you'd like to know more about me!
         </p>
         <a href="mailto:myemail@email.com" className={styles.contactBtn}>
           Contact Me
         </a>
       </div>
-      <img
-        src={getImageUrl("hero/logoImage.png")}
-        alt="Hero image of me"
-        className={styles.heroImg}
-      />
+      <img src={getImageUrl("hero/logoImage.png")} alt="Hero image of me" className={styles.heroImg}/>
       <div className={styles.topBlur} />
       <div className={styles.bottomBlur} />
     </section>
