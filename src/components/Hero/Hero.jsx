b4c9b13d3e0b23b1915ec9271ca41b075ee2f396
joelsrzalvarez <contactojoelsuarez@gmail.com>
2024-06-08 23:42:49 +0200

    presentation in english and spanish

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,22 +1,27 @@
 import React from "react";
-
 import styles from "./Hero.module.css";
 import { getImageUrl } from "../../utils";
+import { useLanguage } from '../../context/LanguageContext'; 
 
 export const Hero = () => {
+  const { language } = useLanguage(); 
+
   return (
     <section className={styles.container}>
       <div className={styles.content}>
-        <h1 className={styles.title}>Hi, I'm Joel</h1>
+        <h1 className={styles.title}>
+          {language === 'english' ? "Hi, I'm Joel" : "Hola, soy Joel"}
+        </h1>
         <p className={styles.description}>
-          I'm a full-stack developer with almost 2 years of experience using JavaScript. 
-          Reach out if you'd like to know more about me!
+          {language === 'english' 
+            ? "I'm a full-stack developer with almost 2 years of experience using JavaScript. Reach out if you'd like to know more about me!"
+            : "Soy un desarrollador full-stack con casi 2 años de experiencia utilizando JavaScript. ¡No dudes en contactarme si quieres saber más sobre mí!"}
         </p>
         <a href="mailto:myemail@email.com" className={styles.contactBtn}>
-          Contact Me
+          {language === 'english' ? "Contact Me" : "Contáctame"}
         </a>
       </div>
-      <img src={getImageUrl("hero/logoImage.png")} alt="Hero image of me" className={styles.heroImg}/>
+      <img src={getImageUrl("hero/logoImage.png")} alt={language === 'english' ? "Hero image of me" : "Imagen de héroe de mí"} className={styles.heroImg}/>
       <div className={styles.topBlur} />
       <div className={styles.bottomBlur} />
     </section>
