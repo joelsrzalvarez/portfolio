75ae3638c4f53a172f8124faca8d0127482a1c44
joelsrzalvarez <contactojoelsuarez@gmail.com>
2024-06-08 23:28:28 +0200

    ABOUT ME in english and spanish

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,36 +1,40 @@
 import React from "react";
-
 import styles from "./About.module.css";
 import { getImageUrl } from "../../utils";
+import { useLanguage } from '../../context/LanguageContext';
 
 export const About = () => {
+  const { language } = useLanguage(); 
+
   return (
     <section className={styles.container} id="about">
-      <h2 className={styles.title}>About</h2>
+      <h2 className={styles.title}>{language === 'english' ? 'About' : 'Sobre mi'}</h2>
       <div className={styles.content}>
         <img
           src={getImageUrl("about/aboutImage.png")}
-          alt="Me sitting with a laptop"
+          alt={language === 'english' ? "Me sitting with a laptop" : "Yo sentado con un portátil"}
           className={styles.aboutImage}
         />
         <ul className={styles.aboutItems}>
           <li className={styles.aboutItem}>
             <img src={getImageUrl("about/cursorIcon.png")} alt="Cursor icon" />
             <div className={styles.aboutItemText}>
-              <h3>Frontend Developer</h3>
+              <h3>{language === 'english' ? 'Frontend Developer' : 'Desarrollador Frontend'}</h3>
               <p>
-                I'm a frontend developer with experience in building responsive
-                and optimized sites
+                {language === 'english' 
+                  ? "I'm a frontend developer with experience in building responsive and optimized sites"
+                  : "Soy un desarrollador frontend con experiencia en la creación de sitios adaptables a todos los dispositivos y optimizados"}
               </p>
             </div>
           </li>
           <li className={styles.aboutItem}>
             <img src={getImageUrl("about/serverIcon.png")} alt="Server icon" />
             <div className={styles.aboutItemText}>
-              <h3>Backend Developer</h3>
+              <h3>{language === 'english' ? 'Backend Developer' : 'Desarrollador Backend'}</h3>
               <p>
-                I have experience developing fast and optimised back-end systems
-                and APIs
+                {language === 'english' 
+                  ? "I have experience developing fast and optimised back-end systems and APIs"
+                  : "Tengo experiencia en el desarrollo de sistemas backend rápidos, optimizados y APIs"}
               </p>
             </div>
           </li>
